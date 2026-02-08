14f246dedfe83d6a12e32e41cbaba0fd64592179
darrenak403 <ngothanhdat4002@gmail.com>
2026-02-08 23:24:10 +0700

    feat(seed): add coupon seeding functionality
    Changes:
    - Implemented SaleSeedData class for seeding coupons
    - Updated Program.cs to call SeedCouponsAsync during migration
    - Added Data\Seeders folder in project structure

--- src/Services/Sale/Beyond8.Sale.Api/Program.cs
+++ src/Services/Sale/Beyond8.Sale.Api/Program.cs
@@ -1,5 +1,6 @@
 using Beyond8.Sale.Api.Bootstrapping;
 using Beyond8.Sale.Infrastructure.Data;
+using Beyond8.Sale.Infrastructure.Data.Seeders;
 using Beyond8.DatabaseMigrationHelpers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,12 @@ builder.AddApplicationServices();
 
 var app = builder.Build();
 
-await app.MigrateDbContextAsync<SaleDbContext>();
+await app.MigrateDbContextAsync<SaleDbContext>(async (database, cancellationToken) =>
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<SaleDbContext>();
+    await SaleSeedData.SeedCouponsAsync(context);
+});
 
 app.UseApplicationServices();
 app.Run();
